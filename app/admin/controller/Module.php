c05441b9e4eeb39b2f132b0fe294045b7913c430
YangHongwei <403236160@qq.com>
2023-11-20 14:37:24 +0800

    feat:模块上传安装时对系统版本、模块互斥和依赖关系进行检测

--- app/admin/controller/Module.php
+++ app/admin/controller/Module.php
@@ -27,7 +27,7 @@ class Module extends Backend
         ]);
     }
 
-    public function state()
+    public function state(): void
     {
         $uid = $this->request->get("uid/s", '');
         if (!$uid) {
@@ -38,7 +38,7 @@ class Module extends Backend
         ]);
     }
 
-    public function install()
+    public function install(): void
     {
         AdminLog::setTitle(__('Install module'));
         $uid     = $this->request->get("uid/s", '');
@@ -60,7 +60,7 @@ class Module extends Backend
         ]);
     }
 
-    public function dependentInstallComplete()
+    public function dependentInstallComplete(): void
     {
         $uid = $this->request->get("uid/s", '');
         if (!$uid) {
@@ -76,7 +76,7 @@ class Module extends Backend
         $this->success();
     }
 
-    public function changeState()
+    public function changeState(): void
     {
         AdminLog::setTitle(__('Change module state'));
         $uid   = $this->request->post("uid/s", '');
@@ -97,7 +97,7 @@ class Module extends Backend
         ]);
     }
 
-    public function uninstall()
+    public function uninstall(): void
     {
         AdminLog::setTitle(__('Unload module'));
         $uid = $this->request->get("uid/s", '');
@@ -114,7 +114,7 @@ class Module extends Backend
         $this->success();
     }
 
-    public function update()
+    public function update(): void
     {
         AdminLog::setTitle(__('Update module'));
         $uid     = $this->request->get("uid/s", '');
@@ -133,16 +133,17 @@ class Module extends Backend
         $this->success();
     }
 
-    public function upload()
+    public function upload(): void
     {
         AdminLog::setTitle(__('Upload install module'));
-        $file = $this->request->get("file/s", '');
-        if (!$file) {
-            $this->error(__('Parameter error'));
-        }
+        $file  = $this->request->get("file/s", '');
+        $token = $this->request->get("token/s", '');
+        if (!$file) $this->error(__('Parameter error'));
+        if (!$token) $this->error(__('Please login to the official website account first'));
+
         $info = [];
         try {
-            $info = Manage::instance()->upload($file);
+            $info = Manage::instance()->upload($token, $file);
         } catch (Exception $e) {
             $this->error(__($e->getMessage()), $e->getData(), $e->getCode());
         } catch (Throwable $e) {
--- app/admin/lang/zh-cn/module.php
+++ app/admin/lang/zh-cn/module.php
@@ -3,6 +3,7 @@ return [
     'Order not found'                                           => '订单找不到啦！',
     'Module already exists'                                     => '模块已存在！',
     'package download failed'                                   => '包下载失败！',
+    'package check failed'                                      => '包检查失败！',
     'No permission to write temporary files'                    => '没有权限写入临时文件！',
     'Zip file not found'                                        => '找不到压缩包文件',
     'Unable to open the zip file'                               => '无法打开压缩包文件',
@@ -23,4 +24,5 @@ return [
     'Update module'                                             => '更新模块',
     'Change module state'                                       => '改变模块状态',
     'Upload install module'                                     => '上传安装模块',
+    'Please login to the official website account first'        => '请先使用BuildAdmin官网账户登录到模块市场~',
 ];
