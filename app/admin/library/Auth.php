08b71847660fd525cf653c92a43eb37c66b60f61
YangHongwei <403236160@qq.com>
2025-03-03 13:50:48 +0800

    style:格式化代码、优化注释

--- app/admin/library/Auth.php
+++ app/admin/library/Auth.php
@@ -439,18 +439,18 @@ class Auth extends \ba\Auth
     }
 
     /**
-     * 获取拥有"所有权限"的分组
-     * @param string $dataLimit 数据权限
-     * @param array $where 分组条件,默认启用的分组 [['status','=',1]]
+     * 获取拥有 `所有权限` 的分组
+     * @param string $dataLimit       数据权限
+     * @param array  $groupQueryWhere 分组查询条件（默认查询启用的分组：[['status','=',1]]）
      * @return array 分组数组
      * @throws Throwable
      */
-    public function getAllAuthGroups(string $dataLimit,array $where = [['status','=',1]]): array
+    public function getAllAuthGroups(string $dataLimit, array $groupQueryWhere = [['status', '=', 1]]): array
     {
         // 当前管理员拥有的权限
         $rules         = $this->getRuleIds();
         $allAuthGroups = [];
-        $groups        = AdminGroup::where($where)->select();
+        $groups        = AdminGroup::where($groupQueryWhere)->select();
         foreach ($groups as $group) {
             if ($group['rules'] == '*') {
                 continue;
